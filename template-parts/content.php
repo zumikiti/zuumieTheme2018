95dec5e28855d3e1aa53105a5ca908f34d8ed5c5
zumikiti <aponmaru@gmail.com>
2018-09-21 23:00:16 +0900

    コンテンツをカードへ変更。 the_content(); から the_excerpt(); へ変更

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,51 +9,50 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header bg-primary">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta bg-secondary">
-				<?php
-				zuumietheme2018_posted_on();
-				zuumietheme2018_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+<div class="col-md-6">
+	<div class="card-group">
+		<div id="post-<?php the_ID(); ?>" class="card">
 
-	<?php zuumietheme2018_post_thumbnail(); ?>
+			<?php zuumietheme2018_post_thumbnail(); ?>
 
-	<div class="entry-content bg-success">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'zuumietheme2018' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+			<div class="card-body">
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuumietheme2018' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+				<div class="card-title">
+					<?php
+					if ( is_singular() ) :
+						the_title( '<h1 class="h5">', '</h1>' );
+					else :
+						the_title( '<h2 class="h5"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					endif;
+					?>
+				</div><!-- .entry-header -->
 
-	<footer class="entry-footer bg-info">
-		<?php zuumietheme2018_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+				<div class="card-text small">
+					<?php
+					the_excerpt();
+
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuumietheme2018' ),
+						'after'  => '</div>',
+					) );
+					?>
+				</div><!-- .entry-content -->
+
+				<div class="card-footer small">
+					<?php
+					if ( 'post' === get_post_type() ) :
+					?>
+						<div class="entry-meta">
+							<?php
+							the_time('y/m/j g:i A');
+							the_tags('<p class="tags">', ' ', '</p>');
+							?>
+						</div><!-- .entry-meta -->
+					<?php endif; ?>
+				</div><!-- .entry-footer -->
+			</div>
+
+		</div><!-- #post-<?php the_ID(); ?> -->
+	</div>
+</div>
