0f942739d75655476e9706dcbdf8b9ecd397bf3f
zumikiti <aponmaru@gmail.com>
2018-09-27 18:03:18 +0900

    contentのcardfooterの文字サイズと色変更

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,49 +10,32 @@
 ?>
 
 
-<div class="col-md-6">
-	<div class="card-group">
-		<div id="post-<?php the_ID(); ?>" class="card">
+<div class="col-md-6 mb-5">
+	<div id="post-<?php the_ID(); ?>" class="card border-white">
 
-			<?php zuumietheme2018_post_thumbnail(); ?>
+		<?php zuumietheme2018_post_thumbnail(); ?>
 
-			<div class="card-body">
+		<div class="card-body m-0 p-0">
 
-				<div class="card-title">
-					<?php
-					if ( is_singular() ) :
-						the_title( '<h1 class="h5">', '</h1>' );
-					else :
-						the_title( '<h2 class="h5"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-					endif;
-					?>
-				</div><!-- .entry-header -->
+			<div class="card-title">
+				<?php
+				if ( is_singular() ) :
+					the_title( '<h1 class="h5">', '</h1>' );
+				else :
+					the_title( '<h2 class="h5"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
+				?>
+			</div><!-- .entry-header -->
 
-				<div class="card-text small">
-					<?php
-					the_excerpt();
+			<div class="card-footer m-0 p-0 bg-while">
+				<?php if ( 'post' === get_post_type() ) : ?>
+						<ul class="list-inline text-muted">
+							<li class="list-inline-item content-card-footer-list"><?php the_category(','); ?></li>
+							<li class="list-inline-item content-card-footer-list"><?php the_time('Y/m/j'); ?></li>
+						</ul>
+				<?php endif; ?>
+			</div><!-- .entry-footer -->
+		</div>
 
-					wp_link_pages( array(
-						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'zuumietheme2018' ),
-						'after'  => '</div>',
-					) );
-					?>
-				</div><!-- .entry-content -->
-
-				<div class="card-footer small">
-					<?php
-					if ( 'post' === get_post_type() ) :
-					?>
-						<div class="entry-meta">
-							<?php
-							the_time('y/m/j g:i A');
-							the_tags('<p class="tags">', ' ', '</p>');
-							?>
-						</div><!-- .entry-meta -->
-					<?php endif; ?>
-				</div><!-- .entry-footer -->
-			</div>
-
-		</div><!-- #post-<?php the_ID(); ?> -->
-	</div>
+	</div><!-- #post-<?php the_ID(); ?> -->
 </div>
